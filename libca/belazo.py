cdcb8a4ccb8fa5b37d71a800d49c0c658c5d2ec9
OsipenkoMV <maksosipenko17@gmail.com>
2025-04-03 15:30:16 +0300

    новая функция: реализация шифрования Белазо

---
+++ libca/belazo.py
@@ -0,0 +1,16 @@
+# --------------------------------------------------
+# MIT License
+# Copyright (c) 2025 Maks Osipenko
+# --------------------------------------------------
+
+
+from .alpha import RU32
+
+def periodic(key:str, target_length:int)->str:
+    return key*(target_length//len(key))+key[:target_length-target_length//len(key)]
+
+def belazo(text:str, key:str)->str:
+    ciphertext = str()
+    for letter, symbol in zip(text, periodic(key, len(text))):
+        ciphertext += RU32[(RU32.index(letter)+RU32.index(symbol))%32]
+    return ciphertext

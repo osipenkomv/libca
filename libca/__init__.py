6156569c435bc50d9081735ab65f652acfdee6ba
OsipenkoMV <maksosipenko17@gmail.com>
2025-04-03 15:42:53 +0300

    новая функция: добавить файл инициализации пакета __init__.py

---
+++ libca/__init__.py
@@ -0,0 +1,9 @@
+from .atbash import atbash
+from .caesar import caesar
+from .polybius import polybius
+from .trithemius import trithemius
+from .belazo import belazo
+from .vigenere import vigenere
+from .magma import magma
+
+__all__ = ["atbash", "caesar", "polybius", "trithemius", "belazo", "vigenere", "magma"]

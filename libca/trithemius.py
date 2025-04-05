11349bf4cbc97df6797e7d2db2f9a3a48a9b3743
OsipenkoMV <maksosipenko17@gmail.com>
2025-04-05 22:24:47 +0300

    новая функция: реализация расшифрования Тритемий

--- libca/trithemius.py
+++ libca/trithemius.py
@@ -11,3 +11,9 @@ def trithemius(text:str)->str:
     for index in range(len(text)):
         ciphertext += RU32[(RU32.index(text[index])+index+1)%32]
     return ciphertext
+
+def un_trithemius(ciphertext:str)->str:
+    text = str()
+    for index in range(len(ciphertext)):
+        text += RU32[(RU32.index(ciphertext[index])-index-1)%32]
+    return text

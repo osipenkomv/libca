42f1e28585d91f226952f0a1ef0f5f96d49ee856
OsipenkoMV <maksosipenko17@gmail.com>
2025-04-05 22:22:10 +0300

    новая функция: реализация расшифрования Цезарь

--- libca/caesar.py
+++ libca/caesar.py
@@ -11,3 +11,9 @@ def caesar(text:str, key:int)->str:
     for letter in text:
         ciphertext += RU32[(RU32.index(letter)+key)%32]
     return ciphertext
+
+def un_caesar(ciphertext:str, key:int)->str:
+    text = str()
+    for letter in ciphertext:
+        text += RU32[(RU32.index(letter)-key)%32]
+    return text

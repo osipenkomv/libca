5fb4f535cab8b4423dd03b3773710f2122b49a0d
OsipenkoMV <maksosipenko17@gmail.com>
2025-04-05 22:25:49 +0300

    новая функция: реализация расшифрования Белазо

--- libca/belazo.py
+++ libca/belazo.py
@@ -14,3 +14,9 @@ def belazo(text:str, key:str)->str:
     for letter, symbol in zip(text, periodic(key, len(text))):
         ciphertext += RU32[(RU32.index(letter)+RU32.index(symbol))%32]
     return ciphertext
+
+def un_belazo(ciphertext:str, key:str)->str:
+    text = str()
+    for letter, symbol in zip(ciphertext, periodic(key, len(ciphertext))):
+        text += RU32[(RU32.index(letter)-RU32.index(symbol))%32]
+    return text

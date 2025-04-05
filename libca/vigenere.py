ab5bf8e7916f34c7a171aad53dcf9b435f47f83e
OsipenkoMV <maksosipenko17@gmail.com>
2025-04-05 22:26:48 +0300

    новая функция: реализация расшифрования Виженер

--- libca/vigenere.py
+++ libca/vigenere.py
@@ -11,3 +11,11 @@ def vigenere(text:str, key:str)->str:
     for letter, symbol in zip(text, key+text[:-1]):
         ciphertext += RU32[(RU32.index(letter)+RU32.index(symbol))%32]
     return ciphertext
+
+def un_vigenere(ciphertext:str, key:str)->str:
+    text = str()
+    symbol = key
+    for letter in ciphertext:
+        symbol = RU32[(RU32.index(letter)-RU32.index(symbol))%32]
+        text += symbol
+    return text

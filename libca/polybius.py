a3ce29ac610fbc0edc540898616f222f1bf22ce5
OsipenkoMV <maksosipenko17@gmail.com>
2025-04-05 22:23:24 +0300

    новая функция: реализация расшифрования Полибий

--- libca/polybius.py
+++ libca/polybius.py
@@ -12,3 +12,9 @@ def polybius(text:str)->list[tuple[int, int]]:
         designation = (RU32.index(letter)//6+1, RU32.index(letter)%6+1)
         ciphertext.append(designation)
     return ciphertext
+
+def un_polybius(ciphertext:list[tuple[int, int]])->str:
+    text = str()
+    for designation in ciphertext:
+        text += RU32[(designation[0]-1)*6+(designation[1]-1)]
+    return text
